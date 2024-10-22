bf8b5a9bf7a700e5374f0397e622338103b73443
NicholasCarreiroSecE <n01492047@humbermail.ca>
2024-10-22 16:19:35 -0400

    styling and question adding
    styled the quiz and begun adding questions and logic behind correct and incorrect answers

---
+++ quiz.js
@@ -0,0 +1,11 @@
+const questions = [
+    {
+        question: "What is the largest desert in the world?",
+        answers: [
+            {text: "Sahara Desert", correct: false},
+            {text: "Gobi Desert", correct: false},
+            {text: "Arabian Desert", correct: false},
+            {text: "Antarctic Desert", correct: true},
+        ]   
+    },
+]
